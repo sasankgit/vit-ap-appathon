0ab4f734e17afe6228a033fcb825db3e2bda3eb7
sasankgit <sasank4pr@gmail.com>
2025-10-28 23:36:31 +0530

    added nothin actually

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/login'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path = '/' element={<Home/>}/>
+      <Route path ='/login' element={<Login/>}/>
     </Routes>
     </BrowserRouter>
 
---
+++ src/pages/login.jsx
@@ -0,0 +1,8 @@
+function Login(){
+    return(
+        <div>
+            <h1>this is login page</h1>
+        </div>
+    )
+}
+export default Login
