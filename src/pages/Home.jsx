23335fa99d5440f8acd4f8b4a8f729eff731310f
sasankgit <sasank4pr@gmail.com>
2025-10-28 23:30:56 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        
+        <div className="bg-violet-400 h-10 w-full  border-2 border-red-400 rounded-md my-4 p-1 flex justify-center items-center">
+            <h1 className="text-center font-mono font-extrabold">
+                hello world
+            </h1>
+        </div>
+    )
+}
+export default Home
